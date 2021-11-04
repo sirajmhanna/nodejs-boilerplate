8004f6816cd8432b0920c1a79f24997ddd234148
Siraj <sirajmhanna@hotmail.com>
2021-11-04 15:25:33 +0200

    Create server.js

---
+++ api/controllers/server.js
@@ -0,0 +1,28 @@
+const logger = require('../../helpers/winston');
+
+/**
+ * Server Health Controller
+ * @method GET
+ * @example
+ * {
+    "status": "success",
+    "message": "Server is alive",
+    "data": {
+    "service": "authentication"
+            }
+ * }
+ * 
+ * @param { Object } req 
+ * @param { Object } res 
+ * @returns { Object }
+ */
+exports.health = (req, res) => {
+    logger.info(req.requestID, 'server', 'health', 'Checking server health', { ipAddress: req.ip });
+    return res.status(200).json({
+        status: 'success',
+        message: 'Server is alive',
+        data: {
+            service: process.env.SERVICE_NAME
+        }
+    });
+};
