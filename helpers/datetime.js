b20a88c2f2449eb38dea37f697bf5994da6c9306
Siraj <sirajmhanna@hotmail.com>
2021-11-04 15:25:46 +0200

    Create datetime.js

---
+++ helpers/datetime.js
@@ -0,0 +1,14 @@
+/**
+ * This function returns the date (YYYY-MM-DD)
+ * @function millisecondsToYMD()
+ * @param { number } milliseconds 
+ * @returns { Date }
+ */
+ exports.millisecondsToYMD = (milliseconds) => {
+    const date = new Date(milliseconds);
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+
+    return (`${year}-${month}-${day}`);
+};
