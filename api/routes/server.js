eed8ef52526a8e543c0d2a46bd91825cb3f85d80
Siraj Mhanna <59828433+sirajmhanna@users.noreply.github.com>
2023-07-19 14:19:52 +0300

    Delete server.js

--- api/routes/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const ServerControllers = require("../controllers/server");
-
-// Server Health Route
-route.get("/health", ServerControllers.health);
-
-module.exports = route;
